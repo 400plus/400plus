a5c0db6bca776fdfbd9c4b841db4d83e031e88c1
Eduardo Pérez <eduperez@users.noreply.github.com>
2011-02-04 23:48:52 +0000

    Release 20090902-0

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
-set path=C:\gcc4;c:\gcc4\bin;c:\gcc4\arm-elf;c:\gcc4\arm-elf\bin
+set path=C:\cygwin;c:\cygwin\bin;c:\cygwin\arm-elf;c:\cygwin\arm-elf\bin
+make clean
 make
 make clean
 pause
