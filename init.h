c175bbc67c5d1d397bb8e50bd3a06d1132eca0f4
Eduardo Pérez <eduperez@users.noreply.github.com>
2011-02-09 20:59:09 +0000

    Initialization code clean-up

--- init.h
+++ init.h
@@ -1,14 +1,4 @@
 #ifndef INIT_H_
 #define INIT_H_
 
-extern void COPY();
-
-extern void my_romStart(int startType);
-extern int  my_usrInit(int startType);
-extern int  my_usrRoot(char* pMemPoolStart, unsigned int memPoolSize);
-
-extern void my_taskcreate_Startup();
-void        my_task_Startup();
-int         my_InitializeIntercom();
-
 #endif /* INIT_H_ */
