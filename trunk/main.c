f5b3b6105726f71bcd3169409944f4739dea8438
eperez <eperez@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-02-28 21:50:22 +0000

    Main code clean-up

--- trunk/main.c
+++ trunk/main.c
@@ -6,6 +6,7 @@
 #include "display.h"
 #include "message.h"
 #include "settings.h"
+#include "scripts.h"
 
 #include "main.h"
 
@@ -13,8 +14,6 @@ int* hMyTaskMessQue;
 
 // Temporary storage while displaying ISO at viewfinder
 int viewfinder_iso_CfNotEmitFlash, viewfinder_iso_TvVal;
-
-int interval_original_ae_mode=0;
 
 void  my_IntercomHandler(int r0, char* ptr);
 
@@ -215,12 +214,7 @@ void my_IntercomHandler(int r0, char* ptr) {
 
 void MyTask () {
 	int *pMessage;
-	int dem;
-
-	int m;
-	int av_enc, av_dec, OldAvComp;
-
-	int spotmode = 3, evalue = 0;
+	int spotmode = 3;
 
 	SleepTask(1000);
 	settings_read();
@@ -267,7 +261,7 @@ void MyTask () {
 				break;
 			}
 
-			if (FLAG_FACTORY_DIALOG != 0) {
+			if (FLAG_FACTORY_DIALOG) {
 				MyGlobalStdSet();
 				eventproc_RiseEvent("RequestBuzzer");
 				break;
@@ -319,103 +313,10 @@ void MyTask () {
 			menu_esc();
 			break;
 		case E_AEB:
-			if (settings.eaeb_delay) {
-				eventproc_RiseEvent("RequestBuzzer");
-				SleepTask(2000);
-			}
-
-			if (cameraMode.AEMode == AE_MODE_M) {
-				int m_end;
-				m = settings.eaeb_m_min;
-				do {
-					SendToIntercom(0x8, 1, m);
-					SleepTask(5);
-					eventproc_Release();
-					SleepTask(5);
-					while(FLAG_CAMERA_BUSY)
-						SleepTask(5);
-
-					if(settings.eaeb_m_min == settings.eaeb_m_max) {
-						m_end=m;
-					} else if(settings.eaeb_m_min < settings.eaeb_m_max) {
-						m += 8;
-						m_end = settings.eaeb_m_max + 8;
-					} else {
-						m -= 8;
-						m_end = settings.eaeb_m_max - 8;
-					}
-				} while(m != m_end);
-			} else {
-			  if (cameraMode.CfSettingSteps)
-					settings.eaeb_ev &= 0xFC;
-			  else if((settings.eaeb_ev & 7) != 0 && (settings.eaeb_ev & 3) == 0)
-					settings.eaeb_ev -= 1;
-
-			  OldAvComp = cameraMode.AvComp;
-
-			  av_enc = cameraMode.AvComp;
-			  av_dec = cameraMode.AvComp;
-
-			  eventproc_Release();
-
-			  m = 0;
-			  while (m < (settings.eaeb_frames - 1) / 2) {
-				  av_dec -= settings.eaeb_ev;
-				  av_enc += settings.eaeb_ev;
-
-				  if (cameraMode.CfSettingSteps == 0) {
-					  if ((av_dec & 0x06) == 0x06)
-							av_dec -= 1;
-					  else if((av_dec & 0x07) == 0x02)
-							av_dec += 1;
-
-					  if ((av_enc & 0x06) == 0x06)
-							av_enc -= 1;
-					  else if((av_enc & 0x07) == 0x02)
-							av_enc += 1;
-				  }
-
-				  //if(av_dec<0xCB)av_dec=0xCB;
-				  //if(av_enc>0x30)av_enc=0x30;
-
-				  m++;
-
-				  while (FLAG_CAMERA_BUSY)
-						SleepTask(5);
-
-				  SendToIntercom(0xA, 1, av_dec);
-				  eventproc_Release();
-
-				  while (FLAG_CAMERA_BUSY)
-						SleepTask(5);
-
-				  SendToIntercom(0xA, 1, av_enc);
-				  eventproc_Release();
-			  }
-
-			  SleepTask(500);
-			  SendToIntercom(0xA, 1, OldAvComp);
-			}
-
-			eventproc_RiseEvent("RequestBuzzer");
-			SleepTask(500);
+			script_extended_aeb();
 			break;
 		case INTERVAL:
-			interval_original_ae_mode = cameraMode.AEMode;
-			int i = 0;
-			while (interval_original_ae_mode == cameraMode.AEMode) {
-				while(FLAG_CAMERA_BUSY)
-					SleepTask(5);
-
-				eventproc_Release();
-				for(i = 0; i < settings.interval_time; i++) {
-					if(interval_original_ae_mode == cameraMode.AEMode)
-						SleepTask(1000);
-				}
-			}
-
-			eventproc_RiseEvent("RequestBuzzer");
-
+			script_interval();
 			break;
 		}
 	}
---
+++ trunk/scripts.h
@@ -0,0 +1,9 @@
+#ifndef SCRIPTS_H_
+#define SCRIPTS_H_
+
+#include "scripts.h"
+
+void script_extended_aeb();
+void script_interval();
+
+#endif /* SCRIPTS_H_ */
