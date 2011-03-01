cb7eda28392ae2c3e41fc74421c5fee5d8e25958
Eduardo Pérez <eduperez@users.noreply.github.com>
2011-03-01 21:57:16 +0000

    Factory mode auto-activates debug mode

--- tasks.h
+++ tasks.h
@@ -3,18 +3,17 @@
 
 extern void start_up();
 
-void viewfinder_iso_inc();
-void viewfinder_iso_dec();
-void viewfinder_iso_end();
+extern void viewfinder_iso_inc();
+extern void viewfinder_iso_dec();
+extern void viewfinder_iso_end();
 
-extern void set_intermediate_iso();
-extern void set_metering_spot();
-extern void switch_raw_jpeg();
-extern void show_factory_menu();
-extern void start_debug_mode();
+extern void dp_action();
 
 extern void restore_iso();
 extern void restore_wb();
 extern void restore_metering();
+
+extern void set_metering_spot();
+extern void show_factory_menu();
 
 #endif /* TASKS_H_ */
