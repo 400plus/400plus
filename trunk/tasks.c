00cdb59db6ac46cd5b28b325cea530192831ebbe
eperez <eperez@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-03-01 22:41:12 +0000

    Main code clean-up

--- trunk/tasks.c
+++ trunk/tasks.c
@@ -4,9 +4,6 @@
 
 #include "tasks.h"
 
-// Temporary storage while displaying ISO at viewfinder
-int viewfinder_iso_CfNotEmitFlash, viewfinder_iso_TvVal;
-
 void restore_iso();
 void restore_wb();
 void restore_metering();
@@ -51,41 +48,6 @@ void dp_action() {
 			script_interval();
 			break;
 		}
-	}
-}
-
-void viewfinder_iso_inc() {
-	int iso = iso_inc(cameraMode.ISO);
-
-	if (cameraMode.AEMode == AE_MODE_TV || cameraMode.AEMode == AE_MODE_M) {
-		viewfinder_iso_CfNotEmitFlash = cameraMode.CfNotEmitFlash;
-		SendToIntercom(0x30, 1, 1);
-
-		viewfinder_iso_TvVal = cameraMode.TvVal;
-		SendToIntercom(0x08, 1, iso + 0x25);
-	}
-
-	eventproc_SetIsoValue(&iso);
-}
-
-void viewfinder_iso_dec() {
-	int iso = iso_dec(cameraMode.ISO);
-
-	if (cameraMode.AEMode == AE_MODE_TV || cameraMode.AEMode == AE_MODE_M) {
-		viewfinder_iso_CfNotEmitFlash = cameraMode.CfNotEmitFlash;
-		SendToIntercom(0x30, 1, 1);
-
-		viewfinder_iso_TvVal = cameraMode.TvVal;
-		SendToIntercom(0x08, 1, iso + 0x25);
-	}
-
-	eventproc_SetIsoValue(&iso);
-}
-
-void viewfinder_iso_end() {
-	if (cameraMode.AEMode == AE_MODE_TV || cameraMode.AEMode == AE_MODE_M) {
-		SendToIntercom(0x30, 1, viewfinder_iso_CfNotEmitFlash);
-		SendToIntercom(0x08, 1, viewfinder_iso_TvVal);
 	}
 }
 
---
+++ trunk/viewfinder.h
@@ -0,0 +1,8 @@
+#ifndef VIEWFINDER_H_
+#define VIEWFINDER_H_
+
+extern void viewfinder_iso_inc();
+extern void viewfinder_iso_dec();
+extern void viewfinder_iso_end();
+
+#endif /* VIEWFINDER_H_ */
