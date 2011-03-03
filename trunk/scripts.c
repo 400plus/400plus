8f34c0631b8563c8ce4d0299143709964abe423d
eperez <eperez@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-03-03 23:13:46 +0000

    Use face sensor to stop scripts

--- trunk/scripts.c
+++ trunk/scripts.c
@@ -16,50 +16,49 @@ void script_extended_aeb() {
 	if (settings.eaeb_delay)
 		script_delay(2);
 
-	if (!status.script_running)
-		return;
+	if (! FLAG_FACE_SENSOR) {
+		if (cameraMode.AEMode == AE_MODE_M) {
+			int tv;
 
-	if (cameraMode.AEMode == AE_MODE_M) {
-		int tv;
+			int tv_value = cameraMode.TvVal;
+			int tv_start = MIN(settings.eaeb_m_min, settings.eaeb_m_max);
+			int tv_end   = MAX(settings.eaeb_m_min, settings.eaeb_m_max);
 
-		int tv_value = cameraMode.TvVal;
-		int tv_start = MIN(settings.eaeb_m_min, settings.eaeb_m_max);
-		int tv_end   = MAX(settings.eaeb_m_min, settings.eaeb_m_max);
+			for (tv = tv_start; tv <= tv_end; tv += 8) {
+				SendToIntercom(0x08, 1, tv);
+				release_and_wait();
 
-		for (tv = tv_start; tv <= tv_end; tv += 8) {
-			SendToIntercom(0x08, 1, tv);
+				if (FLAG_FACE_SENSOR)
+					break;
+			};
+
+			SendToIntercom(0x08, 1, tv_value);
+		} else {
+			int av_comp = cameraMode.AvComp;
+			int av_inc  = av_comp;
+			int av_dec  = av_comp;
+
 			release_and_wait();
 
-			if (!status.script_running)
-				break;
-		};
+			int i;
+			for(i = 0; i < (settings.eaeb_frames - 1) / 2; i++) {
+				av_inc = ev_add(av_inc, settings.eaeb_ev);
+				SendToIntercom(0x0A, 1, av_inc);
+				release_and_wait();
 
-		SendToIntercom(0x08, 1, tv_value);
-	} else {
-		int av_comp = cameraMode.AvComp;
-		int av_inc  = av_comp;
-		int av_dec  = av_comp;
+				if (FLAG_FACE_SENSOR)
+					break;
 
-		release_and_wait();
+				av_dec = ev_sub(av_dec, settings.eaeb_ev);
+				SendToIntercom(0x0A, 1, av_dec);
+				release_and_wait();
 
-		int i;
-		for(i = 0; i < (settings.eaeb_frames - 1) / 2; i++) {
-			av_inc = ev_add(av_inc, settings.eaeb_ev);
-			SendToIntercom(0x0A, 1, av_inc);
-			release_and_wait();
+				if (FLAG_FACE_SENSOR)
+					break;
+			}
 
-			if (!status.script_running)
-				break;
-
-			av_dec = ev_sub(av_dec, settings.eaeb_ev);
-			SendToIntercom(0x0A, 1, av_dec);
-			release_and_wait();
-
-			if (!status.script_running)
-				break;
+			SendToIntercom(0x0A, 1, av_comp);
 		}
-
-		SendToIntercom(0x0A, 1, av_comp);
 	}
 
 	beep();
@@ -73,7 +72,7 @@ void script_interval() {
 	beep();
 	status.script_running = TRUE;
 
-	while (status.script_running) {
+	while (! FLAG_FACE_SENSOR) {
 		wait_for_camera();
 		eventproc_Release();
 		script_delay(settings.interval_time);
@@ -96,6 +95,6 @@ void wait_for_camera() {
 void script_delay(int seconds) {
 	int i = SCRIPT_DELAY_REPEAT * seconds;
 
-	while(--i && status.script_running)
+	while(--i && !FLAG_FACE_SENSOR)
 		SleepTask(SCRIPT_DELAY_TIME);
 }
