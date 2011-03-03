8de0606e946a76e4601db0b905718694c03f7d71
eperez <eperez@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-03-03 22:53:48 +0000

    Interruptible scripts

--- trunk/scripts.h
+++ trunk/scripts.h
@@ -1,6 +1,13 @@
 #ifndef SCRIPTS_H_
 #define SCRIPTS_H_
 
+// Time between tries while waiting for camera
+#define WAIT_CAMERA_BUSY 250
+
+// One second while delaying script
+#define SCRIPT_DELAY_REPEAT   4
+#define SCRIPT_DELAY_TIME   250
+
 void script_extended_aeb();
 void script_interval();
 
