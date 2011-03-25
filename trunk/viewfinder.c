d06e9cbe8b055b691e0a29f99cb7c6b444da1eca
eperez <eperez@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-03-25 22:57:12 +0000

    Code clean-up

--- trunk/viewfinder.c
+++ trunk/viewfinder.c
@@ -6,45 +6,45 @@
 #include "viewfinder.h"
 
 // Temporary storage while displaying ISO at viewfinder
-int viewfinder_iso_CfNotEmitFlash, viewfinder_iso_TvVal;
+struct {
+	int TvVal;
+	int CfNotEmitFlash;
+} storage;
+
+void viewfinder_change_iso(const int iso);
 
 void viewfinder_right() {
-	const int iso = iso_inc(cameraMode.ISO);
-
-	if (settings.iso_in_viewfinder) {
-		if (cameraMode.AEMode == AE_MODE_TV || cameraMode.AEMode == AE_MODE_M) {
-			viewfinder_iso_CfNotEmitFlash = cameraMode.CfNotEmitFlash;
-			SendToIntercom(0x30, 1, 1);
-
-			viewfinder_iso_TvVal = cameraMode.TvVal;
-			SendToIntercom(0x08, 1, iso + 0x25);
-		}
-
-		eventproc_SetIsoValue(&iso);
-	}
+	viewfinder_change_iso(iso_inc(cameraMode.ISO));
 }
 
 void viewfinder_left() {
-	const int iso = iso_dec(cameraMode.ISO);
+	viewfinder_change_iso(iso_dec(cameraMode.ISO));
+}
 
+void viewfinder_end() {
+	// Only if active and mode is P, Tv, Av, M
+	if (settings.iso_in_viewfinder && (cameraMode.AEMode == AE_MODE_M || cameraMode.AEMode == AE_MODE_TV)) {
+		// Restore previous state
+		SendToIntercom(0x30, 1, storage.CfNotEmitFlash);
+		SendToIntercom(0x08, 1, storage.TvVal);
+	}
+}
+
+void viewfinder_change_iso(const int iso) {
+	// Only if active and mode is P, Tv, Av, M
 	if (settings.iso_in_viewfinder) {
-		if (cameraMode.AEMode == AE_MODE_TV || cameraMode.AEMode == AE_MODE_M) {
-			viewfinder_iso_CfNotEmitFlash = cameraMode.CfNotEmitFlash;
-			SendToIntercom(0x30, 1, 1);
+		if (cameraMode.AEMode == AE_MODE_M || cameraMode.AEMode == AE_MODE_TV) {
+			// Save current state
+			storage.TvVal          = cameraMode.TvVal;
+			storage.CfNotEmitFlash = cameraMode.CfNotEmitFlash;
 
-			viewfinder_iso_TvVal = cameraMode.TvVal;
+			// Change to Tv=ISO, no flash
+			SendToIntercom(0x30, 1, 1);
 			SendToIntercom(0x08, 1, iso + 0x25);
 		}
 
+		// Set new ISO
 		eventproc_SetIsoValue(&iso);
 	}
 }
 
-void viewfinder_end() {
-	if (settings.iso_in_viewfinder) {
-		if (cameraMode.AEMode == AE_MODE_TV || cameraMode.AEMode == AE_MODE_M) {
-			SendToIntercom(0x30, 1, viewfinder_iso_CfNotEmitFlash);
-			SendToIntercom(0x08, 1, viewfinder_iso_TvVal);
-		}
-	}
-}
