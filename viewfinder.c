d8c931487ef7ee6d736d4055717ffc6e31bbef9e
Eduardo Pérez <eduperez@users.noreply.github.com>
2011-04-05 16:28:29 +0000

    Code clean-up

--- viewfinder.c
+++ viewfinder.c
@@ -22,11 +22,14 @@ void viewfinder_left() {
 }
 
 void viewfinder_end() {
-	// Only if active and mode is P, Tv, Av, M
-	if (settings.iso_in_viewfinder && (cameraMode.AEMode == AE_MODE_M || cameraMode.AEMode == AE_MODE_TV)) {
+	// Only if being displayed
+	if (status.iso_in_viewfinder) {
 		// Restore previous state
 		SendToIntercom(0x30, 1, storage.CfNotEmitFlash);
 		SendToIntercom(0x08, 1, storage.TvVal);
+
+		// Reset flag, viewfinder restored
+		status.iso_in_viewfinder = FALSE;
 	}
 }
 
@@ -41,6 +44,9 @@ void viewfinder_change_iso(const int iso) {
 			// Change to Tv=ISO, no flash
 			SendToIntercom(0x30, 1, 1);
 			SendToIntercom(0x08, 1, iso + 0x25);
+
+			// Set flag to restore viewfinder later
+			status.iso_in_viewfinder = TRUE;
 		}
 
 		// Set new ISO
