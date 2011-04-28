b4fb2fb99e50d7bc07b6a47e8cc947c7deff4b0a
Eduardo Pérez <eduperez@users.noreply.github.com>
2011-04-28 20:49:15 +0000

    Code clean-up

--- menu.h
+++ menu.h
@@ -90,11 +90,15 @@ typedef struct {
 	type_BTN_HANDLER btn_handler;  // routine to proccess the key presses
 } type_MENU;
 
-extern type_LIST bool_list;
-extern type_LIST delay_list;
-extern type_LIST flash_list;
-extern type_LIST action_list;
-extern type_LIST sspeed_list;
+#define OPTIONLIST_DEC(NAME)      extern type_LIST _##NAME##_LIST_;
+#define OPTIONLIST_REF(NAME)      (&_##NAME##_LIST_)
+#define OPTIONLIST_DEF(NAME, ...) static char *_##NAME##_STRINGS_[]={__VA_ARGS__};  type_LIST _##NAME##_LIST_={length:LENGTH(_##NAME##_STRINGS_),data:_##NAME##_STRINGS_};
+
+OPTIONLIST_DEC(bool)
+OPTIONLIST_DEC(delay)
+OPTIONLIST_DEC(flash)
+OPTIONLIST_DEC(action)
+OPTIONLIST_DEC(shutter)
 
 #define MENUITEM_EV(_NAME_, _VALUE_, _ZMO_) \
 	{name:_NAME_, type:MENUITEM_TYPE_EV, {menuitem_ev:{value:_VALUE_, zero_means_off:_ZMO_}}}
@@ -117,11 +121,11 @@ extern type_LIST sspeed_list;
 #define MENUITEM_EVCOMP(_NAME_, _VALUE_) MENUITEM_EV(_NAME_, _VALUE_, FALSE)
 #define MENUITEM_EVSEP( _NAME_, _VALUE_) MENUITEM_EV(_NAME_, _VALUE_, TRUE)
 
-#define MENUITEM_BOOLEAN(_NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, FALSE, &bool_list)
-#define MENUITEM_DELAY(  _NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, FALSE, &delay_list)
-#define MENUITEM_AFFLASH(_NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, TRUE,  &flash_list)
-#define MENUITEM_ACTION( _NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, TRUE,  &action_list)
-#define MENUITEM_SSPEED( _NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, FALSE, &sspeed_list)
+#define MENUITEM_BOOLEAN(_NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, FALSE, OPTIONLIST_REF(bool))
+#define MENUITEM_DELAY(  _NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, FALSE, OPTIONLIST_REF(delay))
+#define MENUITEM_AFFLASH(_NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, TRUE,  OPTIONLIST_REF(flash))
+#define MENUITEM_ACTION( _NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, TRUE,  OPTIONLIST_REF(action))
+#define MENUITEM_SHUTTER(_NAME_, _VALUE_) MENUITEM_ENUM(_NAME_, _VALUE_, FALSE, OPTIONLIST_REF(shutter))
 
 #define MENUITEM_RELEASE(_NAME_, _VALUE_) MENUITEM_INT(_NAME_, _VALUE_, TRUE,     0,     0,   0,   0, FALSE, "%6u")
 #define MENUITEM_CLRTEMP(_NAME_, _VALUE_) MENUITEM_INT(_NAME_, _VALUE_, FALSE, 1800, 11000, 100, 500, FALSE, "%5u")
