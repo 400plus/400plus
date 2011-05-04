0331437bee3ec4a5f6b6789fd073ee2a4eb535a9
firedust@gmail.com <firedust@gmail.com@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-05-04 23:02:55 +0000

    Code clean-up.

--- menu_rename.c
+++ menu_rename.c
@@ -44,7 +44,6 @@ void rename_destroy();
 char *rename_message(int id);
 
 void rename_create(type_MENU *m, char *filename, type_TASK callback) {
-	printf("\nrename_create()\n");
 	rename_filename = filename;
 	rename_callback = callback;
 
@@ -55,7 +54,6 @@ void rename_create(type_MENU *m, char *filename, type_TASK callback) {
 }
 
 void rename_display() {
-	printf("\nrename_display()\n");
 	int i;
 
 	for(i = 0; i < 5; i++)
@@ -70,7 +68,6 @@ void rename_refresh(int line) {
 }
 
 void rename_up() {
-	printf("\nrename_up()\n");
 	if (x != 0) {
 		x--;
 		rename_display();
@@ -78,7 +75,6 @@ void rename_up() {
 }
 
 void rename_down() {
-	printf("\nrename_down()\n");
 	if (x != 4) {
 		x++;
 		rename_display();
@@ -86,22 +82,18 @@ void rename_down() {
 }
 
 void rename_right() {
-	printf("\nrename_right()\n");
 	rename_repeat(rename_repeateable_right);
 }
 
 void rename_left() {
-	printf("\nrename_left()\n");
 	rename_repeat(rename_repeateable_left);
 }
 
 void rename_cycle() {
-	printf("\nrename_cycle()\n");
 	rename_repeat(rename_repeateable_cycle);
 }
 
 void rename_action() {
-	printf("\nrename_action()\n");
 	if (x < 4) {
 		rename_filename[z] = letters[caps][x][y];
 
@@ -119,7 +111,6 @@ void rename_action() {
 void rename_clear() {
 	int i;
 
-	printf("\nrename_clear()\n");
 	for (i = z; i < 25; i++)
 		rename_filename[i] = ' ';
 
@@ -127,7 +118,6 @@ void rename_clear() {
 }
 
 void rename_next() {
-	printf("\nrename_next()\n");
 	if (z != 24) {
 		z++;
 		rename_refresh(4);
@@ -135,7 +125,6 @@ void rename_next() {
 }
 
 void rename_prev() {
-	printf("\nrename_prev()\n");
 	if (z != 0) {
 		z--;
 		rename_refresh(4);
@@ -146,7 +135,6 @@ void rename_repeat(void(*repeateable)()){
 	int delay;
 	int button = status.button_down;
 
-	printf("\nrename_repeat()\n");
 	SleepTask(50);
 
 	repeateable(FALSE);
@@ -196,7 +184,6 @@ void rename_repeateable_cycle(int repeating) {
 }
 
 void rename_destroy() {
-	printf("\nrename_destroy()\n");
 	menu->in_rename = 0;
 	menu = 0;
 	x = y = z = 0;
