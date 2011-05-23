71785f52c45880d9569c55a82688364d32fcdbc7
eperez <eperez@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-05-23 22:28:46 +0000

    9 presets instead of 5

--- trunk/menu_presets.c
+++ trunk/menu_presets.c
@@ -16,6 +16,10 @@ void preset_save_2();
 void preset_save_3();
 void preset_save_4();
 void preset_save_5();
+void preset_save_6();
+void preset_save_7();
+void preset_save_8();
+void preset_save_9();
 
 void preset_save(int id);
 
@@ -24,6 +28,10 @@ void preset_load_2();
 void preset_load_3();
 void preset_load_4();
 void preset_load_5();
+void preset_load_6();
+void preset_load_7();
+void preset_load_8();
+void preset_load_9();
 
 void preset_load(int id);
 
@@ -32,7 +40,11 @@ type_MENUITEM presets_save_items[] = {
 	MENUITEM_TASK (presets_config.names[1], preset_save_2),
 	MENUITEM_TASK (presets_config.names[2], preset_save_3),
 	MENUITEM_TASK (presets_config.names[3], preset_save_4),
-	MENUITEM_TASK (presets_config.names[4], preset_save_5)
+	MENUITEM_TASK (presets_config.names[4], preset_save_5),
+	MENUITEM_TASK (presets_config.names[5], preset_save_6),
+	MENUITEM_TASK (presets_config.names[6], preset_save_7),
+	MENUITEM_TASK (presets_config.names[7], preset_save_8),
+	MENUITEM_TASK (presets_config.names[8], preset_save_9)
 };
 
 type_MENUITEM presets_load_items[] = {
@@ -40,7 +52,11 @@ type_MENUITEM presets_load_items[] = {
 	MENUITEM_TASK (presets_config.names[1], preset_load_2),
 	MENUITEM_TASK (presets_config.names[2], preset_load_3),
 	MENUITEM_TASK (presets_config.names[3], preset_load_4),
-	MENUITEM_TASK (presets_config.names[4], preset_load_5)
+	MENUITEM_TASK (presets_config.names[4], preset_load_5),
+	MENUITEM_TASK (presets_config.names[5], preset_load_6),
+	MENUITEM_TASK (presets_config.names[6], preset_load_7),
+	MENUITEM_TASK (presets_config.names[7], preset_load_8),
+	MENUITEM_TASK (presets_config.names[8], preset_load_9)
 };
 
 type_MENU menu_presets_save = {
@@ -76,25 +92,23 @@ void menu_presets_load_start() {
 		menu_create(&menu_presets_load);
 }
 
-void preset_save_1() {
-	preset_save(1);
-}
+void preset_save_1() { preset_save(1); }
 
-void preset_save_2() {
-	preset_save(2);
-}
+void preset_save_2() { preset_save(2); }
 
-void preset_save_3() {
-	preset_save(3);
-}
+void preset_save_3() { preset_save(3); }
 
-void preset_save_4() {
-	preset_save(4);
-}
+void preset_save_4() { preset_save(4); }
 
-void preset_save_5() {
-	preset_save(5);
-}
+void preset_save_5() { preset_save(5); }
+
+void preset_save_6() { preset_save(6); }
+
+void preset_save_7() { preset_save(7); }
+
+void preset_save_8() { preset_save(8); }
+
+void preset_save_9() { preset_save(9); }
 
 void preset_save(int id) {
 	beep();
@@ -104,25 +118,23 @@ void preset_save(int id) {
 	menu_close();
 }
 
-void preset_load_1() {
-	preset_load(1);
-}
+void preset_load_1() { preset_load(1); }
 
-void preset_load_2() {
-	preset_load(2);
-}
+void preset_load_2() { preset_load(2); }
 
-void preset_load_3() {
-	preset_load(3);
-}
+void preset_load_3() { preset_load(3); }
 
-void preset_load_4() {
-	preset_load(4);
-}
+void preset_load_4() { preset_load(4); }
 
-void preset_load_5() {
-	preset_load(5);
-}
+void preset_load_5() { preset_load(5); }
+
+void preset_load_6() { preset_load(6); }
+
+void preset_load_7() { preset_load(7); }
+
+void preset_load_8() { preset_load(8); }
+
+void preset_load_9() { preset_load(9); }
 
 void preset_load(int id) {
 	beep();
