3361e0d35724fbbf49f45f455ae0472b831c42bc
eperez <eperez@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-05-23 22:07:23 +0000

    Set white balance to custom if color temperature is changed (fixes issue #71).

--- trunk/settings.c
+++ trunk/settings.c
@@ -76,6 +76,9 @@ void settings_write() {
 }
 
 extern void settings_apply() {
+	if (settings.color_temp != cameraMode.color_temp)
+		send_to_intercom(IC_SET_WB, 1, WB_MODE_COLORTEMP);
+
 	send_to_intercom(IC_SET_AV_COMP,         1,  settings.av_comp);
 	send_to_intercom(IC_SET_EFCOMP,          1,  settings.flash_comp);
 	send_to_intercom(IC_SET_AE_BKT,          1,  settings.aeb_ev);
