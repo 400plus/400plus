d30c770620446342981642dff779b8c1b82fb47a
eperez <eperez@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-06-06 21:05:54 +0000

    New menu back-end: all shortcuts apply immediately

--- trunk/menu_shortcuts.c
+++ trunk/menu_shortcuts.c
@@ -11,7 +11,10 @@
 
 type_CAMERA_MODE sc_cameraMode;
 
-void menu_shortcuts_apply_iso(type_MENUITEM *item);
+void menu_shortcuts_apply_iso                (type_MENUITEM *item);
+void menu_shortcuts_apply_cf_emit_aux        (type_MENUITEM *item);
+void menu_shortcuts_apply_cf_mirror_up_lock  (type_MENUITEM *item);
+void menu_shortcuts_apply_cf_flash_sync_rear (type_MENUITEM *item);
 
 type_MENUITEM menu_shortcut_items[] = {
 	MENUITEM_ISO    (LP_WORD(L_ISO),           &sc_cameraMode.iso, menu_shortcuts_apply_iso),
@@ -19,9 +22,9 @@ type_MENUITEM menu_shortcut_items[] = {
 	MENUITEM_SCRIPT (LP_WORD(L_INTERVALOMETER), script_interval),
 	MENUITEM_SCRIPT (LP_WORD(L_HAND_WAVING),    script_wave),
 	MENUITEM_SCRIPT (LP_WORD(L_SELF_TIMER),     script_self_timer),
-	MENUITEM_AFFLASH(LP_WORD(L_AF_FLASH),      &sc_cameraMode.cf_emit_aux),
-	MENUITEM_BOOLEAN(LP_WORD(L_MIRROR_LOCKUP), &sc_cameraMode.cf_mirror_up_lock),
-	MENUITEM_BOOLEAN(LP_WORD(L_FLASH_2ND_CURT),&sc_cameraMode.cf_flash_sync_rear)
+	MENUITEM_AFFLASH(LP_WORD(L_AF_FLASH),      &sc_cameraMode.cf_emit_aux,        menu_shortcuts_apply_cf_emit_aux),
+	MENUITEM_BOOLEAN(LP_WORD(L_MIRROR_LOCKUP), &sc_cameraMode.cf_mirror_up_lock,  menu_shortcuts_apply_cf_mirror_up_lock),
+	MENUITEM_BOOLEAN(LP_WORD(L_FLASH_2ND_CURT),&sc_cameraMode.cf_flash_sync_rear, menu_shortcuts_apply_cf_flash_sync_rear)
 };
 
 type_MENU menu_shortcuts = {
@@ -61,4 +64,16 @@ void menu_shortcuts_save() {
 
 void menu_shortcuts_apply_iso(type_MENUITEM *item) {
 	send_to_intercom(IC_SET_ISO, 2, *item->parm.menuitem_iso.value);
+}
+
+void menu_shortcuts_apply_cf_emit_aux(type_MENUITEM *item) {
+	send_to_intercom(IC_SET_CF_EMIT_AUX, 1, *item->parm.menuitem_enum.value);
+}
+
+void menu_shortcuts_apply_cf_mirror_up_lock(type_MENUITEM *item) {
+	send_to_intercom(IC_SET_CF_MIRROR_UP_LOCK, 1, *item->parm.menuitem_enum.value);
+}
+
+void menu_shortcuts_apply_cf_flash_sync_rear(type_MENUITEM *item) {
+	send_to_intercom(IC_SET_CF_FLASH_SYNC_REAR, 1, *item->parm.menuitem_enum.value);
 }
