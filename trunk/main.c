d6aed3d4c6ede5fad104472de5345a2d14d4f0c4
draekko1@gmail.com <draekko1@gmail.com@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-07-07 11:34:12 +0000

    Changed AV button to JUMP button to toggle image quality. Now cycles through RAW, RAW/JPEG L, and JPEG L. Works in A-DEP, M, Av, Tv, P, and AUTO on the dial.

--- trunk/main.c
+++ trunk/main.c
@@ -36,7 +36,6 @@ type_ACTION actions_main[]  = {
 	{IC_BUTTON_DOWN,  TRUE,  FALSE,  {restore_wb}},
 	{IC_BUTTON_LEFT,  TRUE,  FALSE,  {restore_metering}},
 	{IC_BUTTON_DP,    FALSE, TRUE,   {dp_action}},
-	{IC_BUTTON_AV,    TRUE,  FALSE,  {toggle_raw_jpeg}},
 	END_OF_LIST
 };
 
