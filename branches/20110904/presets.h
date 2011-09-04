9a7f7db47ca85dc0e3a000d87c1720f6ab1261b4
eperez <eperez@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-09-04 17:05:14 +0000

    Preparing 20110904 release.

---
+++ branches/20110904/presets.h
@@ -0,0 +1,41 @@
+#ifndef PRESETS_H_
+#define PRESETS_H_
+
+#include "main.h"
+#include "settings.h"
+
+#define PRESETS_VERSION 0x02
+#define PRESETS_CONFIG  "A:/PRESETS"
+#define PRESETS_FILE    "A:/PRESET_%u"
+
+typedef struct {
+	type_CAMERA_MODE camera_mode;
+	type_SETTINGS    settings;
+} type_PRESET;
+
+typedef struct {
+	int  use_adep;
+	int  recall_camera;
+	int  recall_400plus;
+	int  recall_settings;
+	int  recall_image;
+	int  recall_cfn;
+	int  order[9];
+	char names[9][32];
+} type_PRESETS_CONFIG;
+
+extern type_PRESETS_CONFIG presets_config;
+
+extern void presets_read();
+extern void presets_write();
+
+extern int  preset_read (int id);
+extern int  preset_write(int id);
+extern void preset_apply();
+extern void preset_apply_full();
+extern void preset_recall();
+extern void preset_recall_full();
+
+extern void get_preset_filename(char *filename, int id);
+
+#endif /* PRESETS_H_ */
---
+++ branches/20110904/settings.h
@@ -0,0 +1,53 @@
+#ifndef SETTINGS_H_
+#define SETTINGS_H_
+
+#define SETTINGS_FILE     "A:/SETTINGS"
+#define SETTINGS_VERSION  0x24
+
+typedef enum {
+	SHOT_ACTION_SHOT,
+	SHOT_ACTION_EAEB,
+	SHOT_ACTION_INTERVAL,
+	SHOT_ACTION_COUNT,
+	SHOT_ACTION_FIRST = 0,
+	SHOT_ACTION_LAST  = SHOT_ACTION_COUNT - 1
+} type_SHOT_ACTION;
+
+typedef struct {
+	int iso_in_viewfinder;
+	int autoiso_enable;
+	int autoiso_miniso;
+	int autoiso_maxiso;
+	int autoiso_mintv;
+	int autoiso_maxav;
+	int eaeb_frames;
+	int eaeb_delay;
+	int eaeb_ev;
+	int eaeb_tv_min;
+	int eaeb_tv_max;
+	int interval_delay;
+	int interval_time;
+	int interval_eaeb;
+	int interval_shots;
+	int wave_delay;
+	int wave_action;
+	int wave_repeat;
+	int wave_instant;
+	int remote_delay;
+	int timer_timeout;
+	int timer_action;
+	int shortcuts_order[10];
+	int dim_lcd_down;
+	int keep_power_on;
+	int debug_on_poweron;
+	int logfile_mode;
+	int remote_enable;
+} type_SETTINGS;
+
+extern type_SETTINGS settings;
+
+extern int  settings_read();
+extern void settings_write();
+extern void settings_apply();
+
+#endif /* SETTINGS_H_ */
