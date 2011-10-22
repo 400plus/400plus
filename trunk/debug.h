3661aeffe9a249989a2522fb3ca1d21a5ab0a284
firedust@gmail.com <firedust@gmail.com@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-10-22 13:25:06 +0000

    debug stuff and core updates

--- trunk/debug.h
+++ trunk/debug.h
@@ -4,49 +4,22 @@
 #include "firmware.h"
 #include "mainctrl.h"
 
+typedef enum {
+	DEBUG_GENERIC    = 0x00, // +SFACT
+	DEBUG_MAIN       = 0x01, // +MC +TEST
+	DEBUG_STM_SSM    = 0x08, // +AvailShots +Lens +AVS
+	DEBUG_SCP        = 0x09, // +SSMain
+	DEBUG_SDV        = 0x0A, // develop?
+	DEBUG_SRC        = 0x0B,
+	DEBUG_GUI        = 0x18,
+
+} canon_debug_class_t;
+
+
 #ifdef ENABLE_DEBUG
 
 #define debug_log(f, p...) printf_log(8, 8, "[400Plus] %s[%d]: " f, __FILE__, __LINE__, ##p)
 #define debug_printf(f, p...) printf("\n[400Plus] %s[%d]: " f, __FILE__, __LINE__, ##p)
-#if 0
-static inline char * mc_btn_name(char * buf, int event) {
-	switch (event) {
-	case MC_BUTTON_MENU:		sprintf(buf, "MC_BUTTON_MENU");			break;
-	case MC_BUTTON_DISP:		sprintf(buf, "MC_BUTTON_DISP");			break;
-	case MC_BUTTON_JUMP:		sprintf(buf, "MC_BUTTON_JUMP");			break;
-	case MC_BUTTON_PLAY:		sprintf(buf, "MC_BUTTON_PLAY");			break;
-	case MC_BUTTON_TRASH:		sprintf(buf, "MC_BUTTON_TRASH");		break;
-	case MC_BUTTON_SET:		sprintf(buf, "MC_BUTTON_SET");			break;
-	case MC_DIALOG1:		sprintf(buf, "MC_DIALOG1");			break;
-	case MC_DIALOG2:		sprintf(buf, "MC_DIALOG2");			break;
-	case MC_BUTTON_DIAL:		sprintf(buf, "MC_BUTTON_DIAL");			break;
-	case MC_BUTTON_DIAL_LEFT:	sprintf(buf, "MC_BUTTON_DIAL_LEFT");		break;
-	case MC_BUTTON_DIAL_RIGHT:	sprintf(buf, "MC_BUTTON_DIAL_RIGHT");		break;
-	case MC_BUTTON_CARD_DOOR_OPEN:	sprintf(buf, "MC_BUTTON_CARD_DOOR");		break;
-	case MC_BUTTON_UNK1:		sprintf(buf, "MC_BUTTON_UNK1");			break;
-	case MC_BUTTON_POWER:		sprintf(buf, "MC_BUTTON_POWER");		break;
-	case MC_BUTTON_BATTERY_DOOR_OPEN:sprintf(buf, "MC_BUTTON_BATTERY_DOOR");		break;
-	case MC_BUTTON_UP:		sprintf(buf, "MC_BUTTON_UP");			break;
-	case MC_BUTTON_DOWN:		sprintf(buf, "MC_BUTTON_DOWN");			break;
-	case MC_BUTTON_RIGHT:		sprintf(buf, "MC_BUTTON_RIGHT");		break;
-	case MC_BUTTON_LEFT:		sprintf(buf, "MC_BUTTON_LEFT");			break;
-	case MC_BUTTON_HALF_SHUTTER:	sprintf(buf, "MC_BUTTON_HALF_SHUTTER");		break;
-	case MC_BUTTON_FULL_SHUTTER:	sprintf(buf, "MC_BUTTON_FULL_SHUTTER");		break;
-	case MC_BUTTON_DP:		sprintf(buf, "MC_BUTTON_DP");			break;
-	case MC_AFPDLGON:		sprintf(buf, "MC_AFPDLGON");			break;
-	case MC_BUTTON_DRIVE:		sprintf(buf, "MC_BUTTON_DRIVE");		break;
-	case MC_BUTTON_CARD_DOOR_CLOSE:	sprintf(buf, "MC_BUTTON_CF_DOOR_CLOSE");	break;
-	case MC_BUTTON_UNK2:		sprintf(buf, "MC_BUTTON_UNK2");			break;
-	default:			sprintf(buf, "0x%08X", event);			break;
-	}
-	return buf;
-}
-#else
-static inline char * __attribute__((unused)) mc_btn_name(char * buf, int event) {
-	sprintf(buf, "0x%08X", event);
-	return buf;
-}
-#endif
 
 #define LEDRED (*((int*)0xC02200A0))
 #define LEDBLUE (*((int*)0xC0220000))
@@ -61,10 +34,7 @@ static inline char * __attribute__((unused)) mc_btn_name(char * buf, int event) 
 
 #define debug_log(...)
 #define debug_printf(...)
-static inline char * __attribute__((unused)) mc_btn_name(char * buf, int event) {
-	sprintf(buf, "0x%08X", event);
-	return buf;
-}
+
 #endif
 
 
--- trunk/mainctrl.h
+++ trunk/mainctrl.h
@@ -4,6 +4,7 @@
 typedef enum {
 	MC_INFOANDCTRL          = 0x01,
 	MC_BUTTON               = 0x02,
+	MC_TestBasicChk_arg_F6  = 0x03,
 	MC_ACTION               = 0x04,
 	MC_DISPLAY_MODE         = 0x06,
 	MC_START_MODE           = 0x07,
@@ -16,34 +17,39 @@ typedef enum {
 	MC_REQ_UI_UNLOCK        = 0x17,
 } mc_event_t;
 
+// look at 0xFF81C9F0 in OFW for handler
 typedef enum {
-	MC_BUTTON_MENU                 = 0xA0, //
-	MC_BUTTON_DISP                 = 0xA1, //
-	MC_BUTTON_JUMP                 = 0xA2, //
-	MC_BUTTON_PLAY                 = 0xA4, //
-	MC_BUTTON_TRASH                = 0xA5, //
-	MC_BUTTON_SET                  = 0xA6, //
-	MC_DIALOG1                     = 0xA7, //
-	MC_DIALOG2                     = 0xA8, //
-	MC_BUTTON_DIAL                 = 0xA9, //
+	MC_BUTTON_MENU                 = IC_BUTTON_MENU,
+	MC_BUTTON_DISP                 = IC_BUTTON_DISP,
+	MC_BUTTON_JUMP                 = IC_BUTTON_JUMP,
+	// 0xA3 ?? does something in asm
+	MC_BUTTON_PLAY                 = IC_BUTTON_PLAY,
+	MC_BUTTON_TRASH                = IC_BUTTON_TRASH,
+	MC_BUTTON_SET                  = IC_BUTTON_SET,
+	MC_DIALOG1                     = IC_AFPDLGOFF, // seems like dialog destroy
+	MC_DIALOG2                     = 0xA8, // could be dialog create
+	MC_BUTTON_DIAL                 = IC_BUTTON_DIAL,
+	MC_BUTTON_DIAL1                = IC_BUTTON_DIAL1, // does the same as dial
 	MC_BUTTON_DIAL_LEFT            = ( 1 << 8 ) | IC_BUTTON_DIAL, // Fictitious btn
 	MC_BUTTON_DIAL_RIGHT           = ( 2 << 8 ) | IC_BUTTON_DIAL, // Fictitious btn
-	MC_BUTTON_CARD_DOOR_OPEN       = 0xAB, //
-	MC_BUTTON_UNK1                 = 0xAC, //
-	MC_BUTTON_POWER                = 0xAE, //
-	MC_BUTTON_BATTERY_DOOR_OPEN    = 0xAF, //
-	MC_BUTTON_UP                   = 0xB1, //
-	MC_BUTTON_DOWN                 = 0xB2, //
-	MC_BUTTON_RIGHT                = 0xB3, //
-	MC_BUTTON_LEFT                 = 0xB4, //
-	MC_BUTTON_HALF_SHUTTER         = 0xB6, //
-	MC_BUTTON_FULL_SHUTTER         = 0xB7, //
-	MC_BUTTON_DP                   = 0xB8, //
-	MC_AFPDLGON                    = 0xB9, //
-	MC_BUTTON_DRIVE                = 0xBA, //
-	MC_BUTTON_CARD_DOOR_CLOSE      = 0xBB, //
-	MC_BUTTON_UNK2                 = 0xBC, // calls TFT OFF, i cannot find which button is this
-
+	MC_BUTTON_CARD_DOOR_OPEN       = IC_BUTTON_CARD_DOOR,
+	MC_BUTTON_UNK1                 = IC_BUTTON_UNK1,
+	MC_BUTTON_POWER                = IC_BUTTON_POWER,
+	MC_BUTTON_BATTERY_DOOR_OPEN    = IC_BUTTON_BATTERY_DOOR,
+	// 0xB0 ?? does something in asm
+	MC_BUTTON_UP                   = IC_BUTTON_UP,
+	MC_BUTTON_DOWN                 = IC_BUTTON_DOWN,
+	MC_BUTTON_RIGHT                = IC_BUTTON_RIGHT,
+	MC_BUTTON_LEFT                 = IC_BUTTON_LEFT,
+	// 0xB5 ?? does the same as B0
+	MC_BUTTON_HALF_SHUTTER         = IC_BUTTON_HALF_SHUTTER,
+	MC_BUTTON_FULL_SHUTTER         = IC_BUTTON_FULL_SHUTTER,
+	MC_BUTTON_DP                   = IC_BUTTON_DP,
+	MC_AFPDLGON                    = IC_AFPDLGON,
+	MC_BUTTON_DRIVE                = IC_BUTTON_DRIVE,
+	MC_BUTTON_CARD_DOOR_CLOSE      = IC_BUTTON_AV, // decide which one is the real one
+	MC_BUTTON_AV                   = IC_BUTTON_AV, // probably AV
+	MC_BUTTON_UNK2                 = IC_BUTTON_UNK2, // calls TFT OFF, i cannot find which button is this
 } mc_button_t;
 
 #endif // MAINCTRL_H_
