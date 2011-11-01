08b62e687705c9e08acdecf9617091a6992ad112
Eduardo Pérez <eduperez@users.noreply.github.com>
2011-11-01 22:30:22 +0000

    multi-page menus: use AV + fron wheel to navigate pages

--- menu_main.c
+++ menu_main.c
@@ -3,6 +3,8 @@
 
 #include "languages.h"
 #include "menu.h"
+#include "menupage.h"
+#include "menuitem.h"
 #include "menu_developer.h"
 #include "menu_info.h"
 #include "menu_params.h"
@@ -21,6 +23,20 @@ int  changed;
 void menu_save();
 void menu_set_changed();
 int  menu_get_changed();
+
+void list_display(type_MENU     *menu);
+
+void list_up  (type_MENUPAGE *menupage);
+void list_down(type_MENUPAGE *menupage);
+void list_hide(type_MENUPAGE *menupage);
+
+void goto_params   (type_MENUITEM *item);
+void goto_shortcuts(type_MENUITEM *item);
+void goto_scripts  (type_MENUITEM *item);
+void goto_info     (type_MENUITEM *item);
+void goto_developer(type_MENUITEM *item);
+void goto_settngs  (type_MENUITEM *item);
+void goto_presets  (type_MENUITEM *item);
 
 type_MENUPAGE *menu_main_pages[] = {
 	&menupage_params,
@@ -48,7 +64,30 @@ type_MENU menu_main = {
 		[MENU_EVENT_OUT]    = menu_page_next,
 		[MENU_EVENT_CHANGE] = menu_set_changed,
 		[MENU_EVENT_CLOSE]  = menu_save,
+		[MENU_EVENT_AV]     = list_display,
 	}
+};
+
+type_MENUITEM main_list_items[] = {
+	MENUITEM_LAUNCH("Params",    goto_params),
+	MENUITEM_LAUNCH("Shortcuts", goto_shortcuts),
+	MENUITEM_LAUNCH("Scripts",   goto_scripts),
+	MENUITEM_LAUNCH("Info",      goto_info),
+	MENUITEM_LAUNCH("Developer", goto_developer),
+	MENUITEM_LAUNCH("Settings",  goto_settngs),
+	MENUITEM_LAUNCH("Presets",   goto_presets),
+};
+
+type_MENUPAGE main_list = {
+	name     : "400plus",
+	length   : LENGTH(main_list_items),
+	items    : main_list_items,
+	tasks    : {
+		[MENU_EVENT_NEXT]   = list_up,
+		[MENU_EVENT_PREV]   = list_down,
+		[MENU_EVENT_AV_UP]  = list_hide,
+	},
+	ordering : settings.main_order,
 };
 
 void menu_main_start() {
@@ -82,3 +121,50 @@ int menu_get_changed() {
 	return changed;
 }
 
+void list_display(type_MENU *menu) {
+	menu_set_page(&main_list);
+}
+
+
+void list_up(type_MENUPAGE *menupage) {
+	menu_up(&menu_main);
+}
+
+void list_down(type_MENUPAGE *menupage) {
+	menu_down(&menu_main);
+}
+
+void list_hide(type_MENUPAGE *page) {
+	type_MENUITEM *item = get_current_item(page);
+
+	if (item && item->tasks[MENU_EVENT_SET])
+		item->tasks[MENU_EVENT_SET](item);
+}
+
+void goto_params(type_MENUITEM *item) {
+	menu_set_page(&menupage_params);
+}
+
+void goto_shortcuts(type_MENUITEM *item) {
+	menu_set_page(&menupage_shortcuts);
+}
+
+void goto_scripts(type_MENUITEM *item) {
+	menu_set_page(&menupage_scripts);
+}
+
+void goto_info(type_MENUITEM *item) {
+	menu_set_page(&menupage_info);
+}
+
+void goto_developer(type_MENUITEM *item) {
+	menu_set_page(&menupage_developer);
+}
+
+void goto_settngs(type_MENUITEM *item) {
+	menu_set_page(&menupage_settings);
+}
+
+void goto_presets(type_MENUITEM *item) {
+	menu_set_page(&menupage_presets);
+}
