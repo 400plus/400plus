a9091055c94e048f32a4f4b6734edb123ea9eb18
eduardo.perezesteban <eduardo.perezesteban@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-11-06 22:15:33 +0000

    multi-page menus: i18n

--- trunk/menu_params.c
+++ trunk/menu_params.c
@@ -20,15 +20,15 @@ void menu_params_apply_ae_bkt         (type_MENUITEM *item);
 void menu_params_apply_color_temp     (type_MENUITEM *item);
 
 type_MENUITEM autoiso_items[] = {
-	MENUITEM_BOOLEAN(LP_WORD(L_AUTOISO_ENABLE), &settings.autoiso_enable, NULL),
-	MENUITEM_BASEISO(LP_WORD(L_AUTOISO_MINISO), &settings.autoiso_miniso, menu_params_apply_autoiso_miniso),
-	MENUITEM_BASEISO(LP_WORD(L_AUTOISO_MAXISO), &settings.autoiso_maxiso, menu_params_apply_autoiso_maxiso),
-	MENUITEM_TV     (LP_WORD(L_AUTOISO_MINTV),  &settings.autoiso_mintv,  NULL),
-	MENUITEM_AV     (LP_WORD(L_AUTOISO_MAXAV),  &settings.autoiso_maxav,  NULL),
+	MENUITEM_BOOLEAN(LP_WORD(L_I_AUTOISO_ENABLE), &settings.autoiso_enable, NULL),
+	MENUITEM_BASEISO(LP_WORD(L_I_AUTOISO_MINISO), &settings.autoiso_miniso, menu_params_apply_autoiso_miniso),
+	MENUITEM_BASEISO(LP_WORD(L_I_AUTOISO_MAXISO), &settings.autoiso_maxiso, menu_params_apply_autoiso_maxiso),
+	MENUITEM_TV     (LP_WORD(L_I_AUTOISO_MINTV),  &settings.autoiso_mintv,  NULL),
+	MENUITEM_AV     (LP_WORD(L_I_AUTOISO_MAXAV),  &settings.autoiso_maxav,  NULL),
 };
 
 type_MENUPAGE autoiso_page = {
-	name     : LP_WORD(L_AUTOISO),
+	name     : LP_WORD(L_S_AUTOISO),
 	length   : LENGTH(autoiso_items),
 	items    : autoiso_items,
 	tasks    : {
@@ -37,13 +37,13 @@ type_MENUPAGE autoiso_page = {
 };
 
 type_MENUITEM menupage_params_items[] = {
-	MENUITEM_SUBMENU(LP_WORD(L_AUTOISO),      &autoiso_page,                  NULL),
-	MENUITEM_FULLISO(LP_WORD(L_ISO),          &menu_cameraMode.iso,           menu_params_apply_iso),
-	MENUITEM_EVCOMP (LP_WORD(L_AV_COMP),      &menu_cameraMode.av_comp,       menu_params_apply_av_comp),
-	MENUITEM_EVCOMP (LP_WORD(L_FLASH_COMP),   &menu_cameraMode.efcomp,        menu_params_apply_efcomp),
-	MENUITEM_BOOLEAN(LP_WORD(L_USE_FLASH),    &menu_cameraMode.cf_emit_flash, menu_params_apply_cf_emit_flash),
-	MENUITEM_EVSEP  (LP_WORD(L_AEB),          &menu_cameraMode.ae_bkt,        menu_params_apply_ae_bkt),
-	MENUITEM_CLRTEMP(LP_WORD(L_COLOR_TEMP_K), &menu_cameraMode.color_temp,    menu_params_apply_color_temp),
+	MENUITEM_SUBMENU(LP_WORD(L_S_AUTOISO),      &autoiso_page,                  NULL),
+	MENUITEM_FULLISO(LP_WORD(L_I_ISO),          &menu_cameraMode.iso,           menu_params_apply_iso),
+	MENUITEM_EVCOMP (LP_WORD(L_I_AV_COMP),      &menu_cameraMode.av_comp,       menu_params_apply_av_comp),
+	MENUITEM_EVCOMP (LP_WORD(L_I_FLASH_COMP),   &menu_cameraMode.efcomp,        menu_params_apply_efcomp),
+	MENUITEM_BOOLEAN(LP_WORD(L_I_USE_FLASH),    &menu_cameraMode.cf_emit_flash, menu_params_apply_cf_emit_flash),
+	MENUITEM_EVSEP  (LP_WORD(L_I_AEB),          &menu_cameraMode.ae_bkt,        menu_params_apply_ae_bkt),
+	MENUITEM_CLRTEMP(LP_WORD(L_I_COLOR_TEMP_K), &menu_cameraMode.color_temp,    menu_params_apply_color_temp),
 };
 
 type_MENUPAGE menupage_params = {
