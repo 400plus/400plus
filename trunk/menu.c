d2804df3d13725dbb17a19638cf89b03c370014c
eduardo.perezesteban <eduardo.perezesteban@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2011-11-27 22:18:17 +0000

    Well, perhaps we still do not know how to create a dialog, but we surely know how to destroy one.

--- trunk/menu.c
+++ trunk/menu.c
@@ -61,7 +61,7 @@ void menu_create(type_MENU *menu) {
 	SleepTask(100);
 
 	status.menu_running = TRUE;
-	FLAG_GUI_MODE = 0x2D; // Just a temporary value solution
+	//FLAG_GUI_MODE = 0x2D; // Just a temporary value solution
 
 	current_menu    = menu;
 	menu_cameraMode = *cameraMode;
@@ -86,8 +86,18 @@ void menu_create(type_MENU *menu) {
 }
 
 void menu_close() {
-	press_button(IC_BUTTON_DISP);
-	menu_destroy();
+    GUI_Lock();
+    GUI_PalleteInit();
+
+	DeleteDialogBox(menu_handler);
+	menu_handler = NULL;
+
+	GUI_StartMode(GUIMODE_OLC);
+    CreateDialogBox_OlMain();
+    GUIMode = GUIMODE_OLC;
+
+    GUI_UnLock();
+    GUI_PalleteUnInit();
 }
 
 void menu_initialize() {
