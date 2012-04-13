f504f86151c71f035caa88c16620e3cafaceb3d1
firedust@gmail.com <firedust@gmail.com@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2012-04-13 20:31:42 +0000

    removed old 0xAF branch

--- branches/0xAF/menu_shortcuts.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENU_SHORTCUTS_H_
-#define MENU_SHORTCUTS_H_
-
-extern void menu_shortcuts_start();
-extern void menu_shortcuts_create();
-extern void menu_shortcuts_save();
-
-#endif /* MENU_SHORTCUTS_H_ */
