97b4a2dc6921f58e6ce7800b1a704401288cc585
eduardo.perezesteban <eduardo.perezesteban@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2012-04-15 20:46:34 +0000

    Release 20120415-0

---
+++ tags/20120415-0/tasks.h
@@ -0,0 +1,28 @@
+#ifndef TASKS_H_
+#define TASKS_H_
+
+extern void start_up();
+
+extern void dp_action();
+
+extern void toggle_img_format();
+extern void toggle_CfMLU();
+extern void toggle_CfFlashSyncRear();
+extern void toggle_AEB();
+
+extern void restore_iso();
+extern void restore_wb();
+extern void restore_metering();
+
+extern void set_intermediate_iso();
+extern void set_metering_spot();
+extern void set_whitebalance_colortemp();
+
+extern void autoiso();
+extern void autoiso_enable();
+extern void autoiso_disable();
+
+extern void button_jump_task();
+extern void button_trash_task();
+
+#endif /* TASKS_H_ */
