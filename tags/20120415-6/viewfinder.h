8184515db202cd8bd929d4ffb61ddde7dd0dbd33
eduardo.perezesteban <eduardo.perezesteban@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2012-05-05 21:52:20 +0000

    Release 20120415-6

---
+++ tags/20120415-6/viewfinder.h
@@ -0,0 +1,10 @@
+#ifndef VIEWFINDER_H_
+#define VIEWFINDER_H_
+
+extern void viewfinder_right();
+extern void viewfinder_left();
+extern void viewfinder_up();
+
+extern void viewfinder_end();
+
+#endif /* VIEWFINDER_H_ */
