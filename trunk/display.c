babfb0ce2e8b0f649a981d05552e41cf5eca9d16
eduardo.perezesteban <eduardo.perezesteban@5d84059a-76c4-28e0-7cfd-9504e5991db9>
2012-12-25 23:32:59 +0000

    Fix: wrong preset name being displayed

--- trunk/display.c
+++ trunk/display.c
@@ -158,5 +158,5 @@ void display_brightness() {
 
 void display_overlay() {
 	if (status.preset_active && presets_config.last_preset != PRESET_NONE)
-		bmp_printf(FONT_SMALL, 16, 96, "%s", presets_config.names[presets_config.last_preset - 1]);
+		bmp_printf(FONT_SMALL, 16, 96, "%s", presets_config.names[presets_config.last_preset]);
 }
