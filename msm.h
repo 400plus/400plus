375a08ff9423e8f759862530ef5a6ddc11b62dd3
Eduardo Pérez <eduperez@users.noreply.github.com>
2013-02-04 21:43:10 +0000

    Multi-spot metering: improve usability when deleting a spot

--- msm.h
+++ msm.h
@@ -11,6 +11,7 @@
 #define MSM_H_
 
 #define MSM_TIMEOUT 2000 // Time (ms) for the DOWN button to become a DELETE
+#define MSM_RETRY    100 // Time (ms) between each check for the DOWN button
 
 void msm_register();
 void msm_release ();
