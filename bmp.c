17877436d69b4cd868e68f079165289e4a460159
Jean THOMAS <contact@tibounise.com>
2015-03-23 19:10:26 +0100

    WIP : screenshot. Now using BMP. Still not there.

--- bmp.c
+++ bmp.c
@@ -15,10 +15,12 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #include "firmware.h"
 #include "bmp.h"
 #include "debug.h"
+#include "cmodes.h"
 
 uint8_t *VramAddrOverride;
 
@@ -154,7 +156,7 @@ void bmp_draw_palette(uint8_t *vram_address) {
 }
 
 void ppm_vram_screenshot(uint8_t *vram_address) {
-    char filename[20] = "A:/SCREENSHOT.PPM";
+    char filename[20] = "A:/SCREENSHOT.BMP";
 
     lcd_printf(0,0,"Vram size : %d",VramSize);
 
@@ -165,7 +167,7 @@ void ppm_vram_screenshot(uint8_t *vram_address) {
     localtime_r(&t, &tm);
 
     // Renaming the screenshot file according to the current date
-    sprintf(filename, "A:/%02d%02d%02d%02d.PPM", tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    sprintf(filename, "A:/%02d%02d%02d%02d.BMP", tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
     // Opening the file
     int file = FIO_OpenFile(filename, O_CREAT | O_WRONLY , 644);
@@ -175,39 +177,128 @@ void ppm_vram_screenshot(uint8_t *vram_address) {
 		beep();
 		beep();
 	} else {
-		FIO_WriteFile(file,"P6\n360 240\n255\n",15);
+		unsigned char bmpfileheader[14] = {'B','M',0,0,0,0,0,0,0,0,54,0,0,0};
+		unsigned char bmpinfoheader[40] = {40,0,0,0,0,0,0,0,0,0,0,0,1,0,24,0};
 
-		int addr = (int)vram_address;
-		int padding = 0;
-		//int power_off_state = DPData.auto_power_off;
+		int height = 240;
+		int width = 360;
+		int filesize = 54 + height*width*3;
+
+		bmpfileheader[2]  = (unsigned char)(filesize);
+		bmpfileheader[3]  = (unsigned char)(filesize >> 8);
+		bmpfileheader[4]  = (unsigned char)(filesize >> 16);
+		bmpfileheader[5]  = (unsigned char)(filesize >> 24);
+
+		bmpinfoheader[4]  = (unsigned char)(width);
+		bmpinfoheader[5]  = (unsigned char)(width >> 8);
+		bmpinfoheader[6]  = (unsigned char)(width >> 16);
+		bmpinfoheader[7]  = (unsigned char)(width >> 24);
+		bmpinfoheader[8]  = (unsigned char)(height);
+		bmpinfoheader[9]  = (unsigned char)(height >> 8);
+		bmpinfoheader[10] = (unsigned char)(height >> 16);
+		bmpinfoheader[11] = (unsigned char)(height >> 24);
+
+		FIO_WriteFile(file,bmpfileheader,14);
+		FIO_WriteFile(file,bmpinfoheader,40);
+
+		// Saving current power state
+		int power_off_state = DPData.auto_power_off;
 
 		// Disabling auto power off
-		//send_to_intercom(IC_SET_AUTO_POWER_OFF, false);
+		send_to_intercom(IC_SET_AUTO_POWER_OFF, false);
 
-		// (360*240)/3 = 28800
-		while (padding < 28800) {
-			// Blink the blue LED
-			LEDBLUE ^= 2;
+		int x,y;
+		for (y = height; y; --y) {
+			char line[1080];
 
-			// One byte buffer
-			char buf[1];
-			char multibuf[3];
-			int i;
-
-			for (i = 0; i < 3; ++i) {
-				memcpy(buf,(void*)(addr+padding+i*360),0x1);
-				multibuf[i] = buf[0];
+			for (x = 0; x < width; ++x) {
+				char current_pixel = vram_address[x+y*360];
+				switch (current_pixel) {
+					case 0x11: // lighter gray
+						line[x*3] = 0xE0;
+						line[x*3+1] = 0xE0;
+						line[x*3+2] = 0xE0;
+						break;
+					case 0x22: // dark gray almost black
+						line[x*3] = 0x26;
+						line[x*3+1] = 0x26;
+						line[x*3+2] = 0x26;
+						break;
+					case 0x33: // light gray
+						line[x*3] = 0xCC;
+						line[x*3+1] = 0xCC;
+						line[x*3+2] = 0xCC;
+						break;
+					case 0x44: // light grey background of menu
+						line[x*3] = 0x7D;
+						line[x*3+1] = 0x7D;
+						line[x*3+2] = 0x7D;
+						break;
+					case 0x55: // light green / lime green
+						line[x*3] = 0x00;
+						line[x*3+1] = 0xFF;
+						line[x*3+2] = 0x8C;
+						break;
+					case 0x66: // red
+						line[x*3] = 0x00;
+						line[x*3+1] = 0x00;
+						line[x*3+2] = 0xFF;
+						break;
+					case 0x77: // brown red / maroon
+						line[x*3] = 0x13;
+						line[x*3+1] = 0x29;
+						line[x*3+2] = 0x61;
+						break;
+					case 0x88: // light blue
+						line[x*3] = 0xFF;
+						line[x*3+1] = 0xE5;
+						line[x*3+2] = 0x00;
+						break;
+					case 0x99: // light gray
+						line[x*3] = 0xCC;
+						line[x*3+1] = 0xCC;
+						line[x*3+2] = 0xCC;
+						break;
+					case 0xAA: // darker gray
+						line[x*3] = 0x40;
+						line[x*3+1] = 0x40;
+						line[x*3+2] = 0x40;
+						break;
+					case 0xBB: // brown red / maroon
+						line[x*3] = 0x13;
+						line[x*3+1] = 0x29;
+						line[x*3+2] = 0x61;
+						break;
+					case 0xCC: // light blue
+						line[x*3] = 0xFF;
+						line[x*3+1] = 0xE5;
+						line[x*3+2] = 0x00;
+						break;
+					case 0xDD: // light orange / pale yellow
+						line[x*3] = 0x00;
+						line[x*3+1] = 0x00;
+						line[x*3+2] = 0xFF;
+						break;
+					case 0xEE: // orange
+						line[x*3] = 0x00;
+						line[x*3+1] = 0x00;
+						line[x*3+2] = 0xFF;
+						break;
+					case 0xFF: // white
+						line[x*3] = 0xFF;
+						line[x*3+1] = 0xFF;
+						line[x*3+2] = 0xFF;
+						break;
+				}
 			}
 
-			FIO_WriteFile(file,multibuf,0x3);
-
-			padding++;
+			FIO_WriteFile(file,line,width*3);
 		}
 
 		FIO_CloseFile(file);
 
-		// Re-enabling auto power off
-		//send_to_intercom(IC_SET_AUTO_POWER_OFF, power_off_state);
+		// Putting back the older power state
+		send_to_intercom(IC_SET_AUTO_POWER_OFF, power_off_state);
 
 		beep();
 	}
