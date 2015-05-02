f138b4286db8f29af4371cc6821cd62f4ee0283d
eduperez <eduardo.perezesteban@gmail.com>
2015-05-02 23:01:42 +0200

    More vxWorks  function calls...

---
+++ vxworks/memLib.h
@@ -0,0 +1,19 @@
+#ifndef VXWORKS_MEMLIB_H_
+#define VXWORKS_MEMLIB_H_
+
+#include "vxworks.h"
+
+typedef struct {} *PART_ID;
+
+STATUS  memPartOptionsSet (PART_ID partId, unsigned options);
+void   *memalign          (unsigned alignment, unsigned size);
+void   *valloc            (unsigned size);
+void   *memPartRealloc    (PART_ID partId, char * pBlock, unsigned nBytes);
+int     memPartFindMax    (PART_ID partId);
+void    memOptionsSet     (unsigned options);
+void   *calloc            (size_t elemNum, size_t elemSize);
+void   *realloc           (void * pBlock, size_t newSize);
+STATUS  cfree             (char * pBlock);
+int     memFindMax        (void);
+
+#endif /* VXWORKS_MEMLIB_H_ */
