cff010b65fd6e58757a47fac5c0cbdeddd2ff459
eduperez <eduardo.perezesteban@gmail.com>
2015-05-06 00:35:27 +0200

    More vxWorks calls

--- vxworks/stdio.h
+++ vxworks/stdio.h
@@ -3,14 +3,49 @@
 
 #include "vxworks.h"
 
-extern int fdprintf  (int fd, const char *fmt, ...);
-extern int printf    (const char *, ...);
-extern int printErr  (const char *fmt, ...);
-extern int sprintf   (char *, const char *, ...);
-extern int sscanf    (const char *, const char *, ...);
-extern int vfdprintf (int fd, const char *fmt, va_list ap);
-extern int vfprintf  (FILE *, const char *, va_list);
-extern int vprintf   (const char *, va_list);
-extern int vsprintf  (char *, const char *, va_list);
+void    clearerr      (FILE *fp);
+int     fclose        (FILE *fp);
+FILE   *fdopen        (int fd, const char *mode);
+int     feof          (FILE *fp);
+int     ferror        (FILE *fp);
+int     fflush        (FILE *fp);
+int     fgetc         (FILE *fp);
+int     fgetpos       (FILE *fp, fpos_t *pos);
+char   *fgets         (char *buf, size_t n, FILE *fp);
+int     fileno        (FILE *fp);
+FILE   *fopen         (const char *file, const char *mode);
+int     fprintf       (FILE *fp, const char *fmt, ...);
+int     fputc         (int c, FILE *fp);
+int     fputs         (const char *s, FILE *fp);
+int     fread         (void *buf, size_t size, size_t count, FILE *fp);
+FILE   *freopen       (const char *file, const char *mode, FILE *fp);
+int     fscanf        (FILE *fp, char const *fmt, ...);
+int     fseek         (FILE *fp, long offset, int whence);
+int     fsetpos       (FILE *iop, const fpos_t *pos);
+long    ftell         (FILE *fp);
+int     fwrite        (const void *buf, size_t size, size_t count, FILE *fp);
+int     getc          (FILE *fp);
+int     getchar       (void);
+char   *gets          (char *buf);
+int     getw          (FILE *fp);
+void    perror        (const char *__s);
+int     putc          (int c, FILE *fp);
+int     putchar       (int c);
+int     puts          (char const *s);
+int     putw          (int w, FILE *fp);
+void    rewind        (FILE *fp);
+int     scanf         (char const *fmt, ...);
+void    setbuf        (FILE *fp, char *buf);
+void    setbuffer     (FILE *fp, char *buf, int size);
+int     setlinebuf    (FILE *fp);
+int     setvbuf       (FILE *fp, char *buf, int mode, size_t size);
+STATUS  stdioInit     (void);
+FILE   *stdioFp       (int stdFd);
+STATUS  stdioShowInit (void);
+STATUS  stdioShow     (FILE *fp, int level);
+FILE   *tmpfile       (void);
+char   *tmpnam        (char *s);
+int     ungetc        (int c, FILE *fp);
+int     vfprintf      (FILE *fp, const char *fmt, va_list vaList);
 
-#endif /* VXWORKS_STDIO_H_ */
+#endif /*VXWORKS_STDIO_H_ */
