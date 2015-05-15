ffc1d33b553dbc448ab328c1b8709316aa628488
eduperez <eduardo.perezesteban@gmail.com>
2015-05-16 00:06:53 +0200

    More VxWorks calls

---
+++ vxworks/tickLib.h
@@ -0,0 +1,10 @@
+#ifndef VXWORKS_TICKLIB_H_
+#define VXWORKS_TICKLIB_H_
+
+#include "vxworks.h"
+
+extern void tickAnnounce (void);
+extern void tickSet      (ULONG ticks);
+extern ULONG tickGet     (void);
+
+#endif /* VXWORKS_TICKLIB_H_ */
